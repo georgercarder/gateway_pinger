fef3e39a1477e7f8a5755ef90d4862d4692ddac6
George Carder <georgercarder@gmail.com>
2022-01-02 10:20:38 -0800

    file and sleepInterval

--- gateway_pinger.sh
+++ gateway_pinger.sh
@@ -2,22 +2,30 @@
 
 gateways=()
 
-cids=()
+if [ -z "$1" ]; then
+        echo "must include file as arg1"
+        exit 1
+fi
+cids=$(awk -F ' ' '{print$2}' $1)
 
-sleepInterval=3600
+if [ -z "$2" ]; then
+        echo "must include sleepInterval as arg2"
+        exit 1
+fi
+sleepInterval=$2
 
 while :
 
 do
 
-  for g in "${gateways[@]}"
-  do
-      for c in "${cids[@]}"
-      do
-          wget --spider $g"/ipfs/"$c &
-      done
-  done
+for g in "${gateways[@]}"
+do
+        for c in ${cids[@]}
+        do
+                wget --spider $g"/ipfs/"$c &
+        done
+done
 
-  sleep $sleepInterval
+sleep $sleepInterval
 
 done
